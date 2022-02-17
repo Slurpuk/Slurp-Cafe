9bc0c33855d7e0189647af1a12a480858fb66b34
sysunia <sonia.koszut@kcl.ac.uk>
2022-02-17 15:39:56 +0000

    set up

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import OrdersPageTester from "./src/testers";
+import FakePage from './src/screens/FakePage'
 
-AppRegistry.registerComponent(appName, () => OrdersPageTester);
+AppRegistry.registerComponent(appName, () => FakePage);
 
 
--- src/screens/FakePage.js
+++ src/screens/FakePage.js
@@ -1,13 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, SafeAreaView, Text, View, StyleSheet, Image} from 'react-native';
-
+import firestore from '@react-native-firebase/firestore';
 
 export const OrdersContext = React.createContext();
 
 const FakePage = () => {
+
+    const fakeOrders = firestore().collection('FakeOrder');
+
     return (
         <SafeAreaView style={{height: '100%'}}>
-            <FlatList data={}>
+            <Text>Orders</Text>
+            <FlatList data={fakeOrders} renderItem={({item}) =>
+                <View>
+                    <Text>item.name</Text>
+                    <Text>item.status</Text>
+                </View>
+            }>
 
             </FlatList>
         </SafeAreaView>
@@ -18,6 +27,6 @@ const styles = StyleSheet.create({
 
 });
 
-export default OrdersPage;
+export default FakePage;
 
 
