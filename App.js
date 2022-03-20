677f6c9c30fe1e96fdf125e7d082bd9001cd9378
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-03-20 06:15:31 +0000

    Add authentication related methods to App and remove depricated files

--- App.js
+++ App.js
@@ -1,15 +1,90 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import auth from '@react-native-firebase/auth';
 import OrdersPage from "./src/screens/OrdersPage";
+import firebase from "@react-native-firebase/app";
+import firestore from "@react-native-firebase/firestore";
+import AuthenticationPage from "./src/screens/AuthenticationPage";
+import {View} from "react-native";
+import AccountManagementPage from "./src/screens/AccountManagementPage";
+//import LogInPage from "./src/screens/LogInPage";
 
 export const GlobalContext = React.createContext();
 
 export default function App() {
-  const [currentUser, setCurrentUser] = useState(auth().currentUser);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [currentUser, setCurrentUser] = useState(auth().currentUser);
+    const [coffeeShopRef, setCoffeeShopRef] = useState(null);
+    const [coffeeShopObj, setCoffeeShopObj] = useState(null);
 
+  useEffect(() => {
+    const subscriber = firestore()
+        .collection('CoffeeShop')
+        .doc(coffeeShopRef)
+        .onSnapshot(documentSnapshot => {
+          setCoffeeShopObj(documentSnapshot.data());
+        });
+
+    // Stop listening for updates when no longer required
+    return () => subscriber();
+  }, [coffeeShopRef]);
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(coffeeShop => {
+      if (coffeeShop) {
+        setIsLoggedIn(true);
+        setCurrentUser(coffeeShop);
+        setCoffeeShop();
+      } else {
+        setIsLoggedIn(false);
+        setCurrentUser(null);
+      }
+    });
+    // Stop listening for updates when no longer required
+    return () => subscriber();
+  });
+
+
+  async function setCoffeeShop() {
+    if (currentUser) {
+      await firestore()
+          .collection('CoffeeShop')
+          .where('Email', '==', currentUser.email)
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(documentSnapshot => {
+              setCoffeeShopRef(documentSnapshot.id);
+            });
+          });
+    }
+  }
+  const Stack = createNativeStackNavigator();
 
   return (
-    <OrdersPage/>
+      <GlobalContext.Provider
+          value={{
+              currentUser: currentUser, // Returns the authentication object
+              coffeeShopRef: coffeeShopRef,
+              coffeeShopObj: coffeeShopObj, // Returns the model object
+          }}
+      >
+        <NavigationContainer>
+          {isLoggedIn ? (
+              <Stack.Navigator
+                  screenOptions={{
+                      headerShown: false,
+                  }}
+              >
+                  <Stack.Screen name="Orders Page" component={OrdersPage} />
+                  <Stack.Screen name="Account Management" component={AccountManagementPage} />
+              </Stack.Navigator>
+          ) : (
+
+              <AuthenticationPage/>
+          )}
+        </NavigationContainer>
+      </GlobalContext.Provider>
   );
 };
 
--- index.js
+++ index.js
@@ -4,11 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import OrdersPageTester from "./src/testers";
-import etaLogic from "./src/screens/etaLogic";
-import ReducedOrder from "./src/components/ReducedOrder";
-import FakePage from './src/screens/FakePage'
-import OrdersPage from "./src/screens/OrdersPage";
 import App from "./App";
 
 AppRegistry.registerComponent(appName, () => App);
