897cbcde5e76887ba4f11612c0b68d9575f63193
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-04-04 06:15:34 +0100

    Set up index.js for using firestore and auth emulators instead of the default cloud service

--- index.js
+++ index.js
@@ -5,6 +5,19 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from "./App";
+import firestore from "@react-native-firebase/firestore";
+import auth from "@react-native-firebase/auth";
+
+const EMULATOR_MODE_ON = true;
+
+if (__DEV__ && EMULATOR_MODE_ON) {
+    try {
+        auth().useEmulator('http://127.0.0.1:9099');
+        firestore().useEmulator('127.0.0.1', 8080);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 AppRegistry.registerComponent(appName, () => App);
 
